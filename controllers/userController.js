3ff24297839d8cc4beff1b5db20fbb040aff3a14
Søren Skieller <soren.skieller@gmail.com>
2019-11-29 14:37:11 +0100

    wat

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,67 +1,68 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 module.exports = {
-  registerNewUser: async (req, res) => {
-    // Register new user
-    try {
-      const user = new User(req.body);
-      // Check if user already existing
-      const query = User.where({ username: user.username });
-      query.findOne(async (err, userAlreadyCreated) => {
-        if (userAlreadyCreated) return res.status(409).send({ error: 'User already created' });
-        // Continue with user registration
-        await user.save();
-        const token = await user.generateAuthToken();
-        res.status(201).send({ user, token });
-      });
+	registerNewUser: async (req, res) => {
+		// Register new user
+		try {
+			const user = new User(req.body);
+			// Check if user already existing
+			const query = User.where({ username: user.username });
+			query.findOne(async (err, userAlreadyCreated) => {
+				if (userAlreadyCreated) return res.status(409).send({ error: "User already created" });
+				// Continue with user registration
+				await user.save();
+				const token = await user.generateAuthToken();
+				res.status(201).send({ user, token });
+			});
 
-    } catch (error) {
-      res.status(400).send(error);
-    };
-  },
+		} catch (error) {
+			res.status(400).send(error);
+		}
+	},
 
-  loginUser: async (req, res) => {
-    // Login registered user
-    try {
-      console.log("logging in")
-      const { username, password } = req.body;
-
+	loginUser: async (req, res) => {
+		// Login registered user
+		try {
+			console.log("logging in");
+			const { username, password } = req.body;
+      console.log(req.body);
       const user = await User.findByCredentials(username, password);
-      if (!user) {
-        return res.status(401).send({ error: 'Login failed! Check authentication credentials' });
-      }
-      const token = await user.generateAuthToken();
-      res.send({ user, token });
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  },
+      console.log("user found")
+			if (!user) {
+				return res.status(401).send({ error: "Login failed! Check authentication credentials" });
+			}
+			const token = await user.generateAuthToken();
+			res.status(200).send({ user, token });
+		} catch (error) {
+			res.status(400).send(error);
+		}
+	},
 
-  logoutUser: async (req, res) => {
-    // Logout user
-    try {
-      req.user.tokens = req.user.tokens.filter((token) => {
-        return token.token != req.token
-      });
+	logoutUser: async (req, res) => {
+		// Logout user
+		try {
+			req.user.tokens = req.user.tokens.filter((token) => {
+				return token.token != req.token;
+			});
 
-      await req.user.save();
-      res.send();
+			await req.user.save();
+			res.send();
 
-    } catch (error) {
-      res.status(500).send(error);
-    };
-  },
+		} catch (error) {
+			res.status(500).send(error);
+		}
+	},
 
-  logoutAll: async (req, res) => {
-    // Logout user from all devices
-    try {
-      req.user.tokens.splice(0, req.user.tokens.length);
+	logoutAll: async (req, res) => {
+		// Logout user from all devices
+		try {
+			req.user.tokens.splice(0, req.user.tokens.length);
 
-      await req.user.save();
-      res.send();
+			await req.user.save();
+			res.send();
 
-    } catch (error) {
-      res.status(500).send(error);
-    };
-  },
-}
+		} catch (error) {
+			res.status(500).send(error);
+		}
+	},
+};
